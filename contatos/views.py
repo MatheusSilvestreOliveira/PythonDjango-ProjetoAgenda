49978c7eaa3837c201d8034a712fd32a78fca77a
Matheus Silvestre <mathsylvester13@gmail.com>
2022-08-24 15:07:35 -0300

    Levantando erro 404 na busca de contatos

--- contatos/views.py
+++ contatos/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Contato
 
 
 # Create your views here.
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = get_list_or_404(Contato)
     return render(request, 'contatos/index.html', {'contatos': contatos})
 
 
 def dados_contato(request, contato_id):
-    contato = Contato.objects.get(id=contato_id)
+    contato = get_object_or_404(Contato, id=contato_id)
     return render(request, 'contatos/dados_contato.html', {'contato': contato})
+
+
