6f82a409e76797927e737fd1e17acaac13714669
Matheus Silvestre <mathsylvester13@gmail.com>
2022-08-24 14:59:05 -0300

    View de detalhes para cada contato

--- contatos/urls.py
+++ contatos/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:contato_id>', views.dados_contato, name='dados_contato'),
 ]
--- contatos/views.py
+++ contatos/views.py
@@ -6,3 +6,8 @@ from .models import Contato
 def index(request):
     contatos = Contato.objects.all()
     return render(request, 'contatos/index.html', {'contatos': contatos})
+
+
+def dados_contato(request, contato_id):
+    contato = Contato.objects.get(id=contato_id)
+    return render(request, 'contatos/dados_contato.html', {'contato': contato})
