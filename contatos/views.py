633932c04d4b167bf2cd3f0fb72a2d1f930f94f3
Matheus Silvestre <mathsylvester13@gmail.com>
2022-08-25 14:02:19 -0300

    Melhorando código com filter

--- contatos/views.py
+++ contatos/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.http import Http404
 from django.core.paginator import Paginator
 from .models import Contato
 
 
 # Create your views here.
 def index(request):
-    contatos = get_list_or_404(Contato)
+    contatos = Contato.objects.order_by('-id').filter(mostrar=True)
     paginator = Paginator(contatos, 10)
     page = request.GET.get('page')
     contatos = paginator.get_page(page)
@@ -14,6 +15,8 @@ def index(request):
 
 def dados_contato(request, contato_id):
     contato = get_object_or_404(Contato, id=contato_id)
+    if not contato.mostrar:
+        raise Http404()
     return render(request, 'contatos/dados_contato.html', {'contato': contato})
 
 
