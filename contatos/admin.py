d173af48f1f62aa3b6eed1e1da1faf11fce48940
Matheus Silvestre <mathsylvester13@gmail.com>
2022-08-25 12:08:27 -0300

    Adicionando campo de mostrar aos contatos

--- contatos/admin.py
+++ contatos/admin.py
@@ -3,11 +3,12 @@ from .models import Categoria, Contato
 
 
 class ContatoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'categoria')
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'categoria', 'mostrar')
     list_display_links = ('id', 'nome', 'sobrenome')
     list_filter = ('nome', 'sobrenome')
     list_per_page = 10
     search_fields = ('nome', 'sobrenome')
+    list_editable = ('telefone', 'mostrar')
 
 
 # Register your models here.
