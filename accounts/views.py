0cbce4665f05ba82dfbe65360b32e98117e6c5fd
Matheus Silvestre <mathsylvester13@gmail.com>
2022-08-29 15:54:17 -0300

    Formulario em models para cadastrar contatos

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages, auth
 from django.core.validators import validate_email
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from .models import FormContato
 
 
 # Create your views here.
@@ -79,4 +80,16 @@ def cadastro(request):
 
 @login_required(redirect_field_name='login')
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    if request.method != 'POST':
+        form = FormContato()
+        return render(request, 'accounts/dashboard.html', {'form': form})
+
+    form = FormContato(request.POST, request.FILES)
+    if not form.is_valid():
+        messages.error(request, 'Erro ao enviar formulário.')
+        form = FormContato(request.POST)
+        return render(request, 'accounts/dashboard.html', {'form': form})
+
+    form.save()
+    messages.success(request, 'Contato cadastrado com sucesso.')
+    return redirect('dashboard')
